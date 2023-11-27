d288ac9a12f41bfeda69478e991a1d6a3b7e1084
elPugna <tomi.pugna@gmail.com>
2023-11-27 22:29:57 +0100

    Chapter 03 - Code 02

---
+++ Chapter 03/Area.java
@@ -0,0 +1,14 @@
+/*
+  - 02 - Page 43 -
+  Compute the area of a circle.
+*/
+class Area {
+  public static void main(String[] args) {
+    double pi, radius, area;
+
+    radius = 10.8; // radius of circle
+    pi = 3.1416; // pi, approximately
+    area = pi * radius * radius; // compute area
+    System.out.println("Area of circle is " + area);
+  }
+}
